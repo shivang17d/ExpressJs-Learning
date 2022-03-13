c8a9660b3f00ffb71ba6d101009cd58dd988bd41
Shivang Gupta <shivangguptaxd@gmail.com>
2022-03-14 01:19:33 +0530

    Add files via upload

---
+++ Phase-3/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');  
+const app = express(); 
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname) + '/index.html')
+})
+
+app.get('/about',(req,res) => {
+    res.sendFile(path.resolve(__dirname) + '/about.html')
+})
+
+app.get('/download',(req,res) => {
+    res.download(path.resolve(__dirname) + '/about.html');
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
